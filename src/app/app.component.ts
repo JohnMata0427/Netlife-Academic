3e6b3e738ee6816bcaad8b6aa3efed721fb65d44
John Mata <john.mata@epn.edu.ec>
2024-06-11 16:30:20 -0500

    Footer Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CourseComponent } from './course/course.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CourseComponent],
+  imports: [RouterOutlet, HeaderComponent, CourseComponent, FooterComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Netlife';
 }
