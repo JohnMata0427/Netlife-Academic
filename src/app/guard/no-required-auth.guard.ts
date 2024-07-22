a2c8939659e8787d6d7546a03b5e334e1580fbbd
JohnMata0427 <john.mata@epn.edu.ec>
2024-07-22 17:12:35 -0500

    Update no-required-auth.guard.ts to redirect authenticated users to the home page

--- src/app/guard/no-required-auth.guard.ts
+++ src/app/guard/no-required-auth.guard.ts
@@ -1,11 +1,13 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@netlifeacademic/services/auth.service';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router)
 
   if (authService.isAuthenticated()) {
+    router.navigate(['/home']);
     return false;
   }
 
